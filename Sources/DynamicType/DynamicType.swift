66569dfb98a1ebd9779e10bcaae31451b532ce7d
Chris Eidhof <chris@eidhof.nl>
2024-04-10 10:24:51 +0200

    Initial commit

---
+++ Sources/DynamicType/DynamicType.swift
@@ -0,0 +1,119 @@
+public enum DynamicTypeStyle: String, Hashable, CaseIterable, Codable {
+    case largeTitle = "Large Title"
+    case title1 = "Title 1"
+    case title2 = "Title 2"
+    case title3 = "Title 3"
+    case headline = "Headline"
+    case body = "Body"
+    case callout = "Callout"
+    case subhead = "Subhead"
+    case footnote = "Footnote"
+    case caption1 = "Caption 1"
+    case caption2 = "Caption 2"
+}
+
+public enum DynamicTypeSize: String, Hashable, CaseIterable {
+    case extraSmall = "Extra Small"
+    case small = "Small"
+    case medium = "Medium"
+    case large = "Large"
+    case extraLarge = "Extra Large"
+    case extraExtraLarge = "Extra Extra Large"
+    case extraExtraExtraLarge = "Extra Extra Extra Large"
+    case accessibilityMedium = "Accessibility Medium"
+    case accessibilityLarge = "Accessibility Large"
+    case accessibilityExtraLarge = "Accessibility Extra Large"
+    case accessibilityExtraExtraLarge = "Accessibility Extra Extra Large"
+    case accessibilityExtraExtraExtraLarge = "Accessibility Extra Extra Extra Large"
+}
+
+extension DynamicTypeStyle {
+    public func value(in instance: DynamicTypeInstance) -> SizeAndLeading {
+        switch self {
+        case .largeTitle:
+            instance.largeTitle
+        case .title1:
+            instance.title1
+        case .title2:
+            instance.title2
+        case .title3:
+            instance.title3
+        case .headline:
+            instance.headline
+        case .body:
+            instance.body
+        case .callout:
+            instance.callout
+        case .subhead:
+            instance.subhead
+        case .footnote:
+            instance.footnote
+        case .caption1:
+            instance.caption1
+        case .caption2:
+            instance.caption2
+        }
+    }
+
+    public func resolve(for style: DynamicTypeSize) -> SizeAndLeading {
+        value(in: style.instance)
+    }
+}
+
+public enum Weight: String, Hashable, CaseIterable, Codable {
+    case regular = "Regular"
+    case semibold = "Semibold"
+    case bold = "Bold"
+}
+
+public struct SizeAndLeading: Hashable, Codable {
+    public var weight: Weight
+    public var size: Double
+    public var leading: Double
+}
+
+public struct DynamicTypeInstance: Hashable, Codable {
+    public var largeTitle: SizeAndLeading
+    public var title1: SizeAndLeading
+    public var title2: SizeAndLeading
+    public var title3: SizeAndLeading
+    public var headline: SizeAndLeading
+    public var body: SizeAndLeading
+    public var callout: SizeAndLeading
+    public var subhead: SizeAndLeading
+    public var footnote: SizeAndLeading
+    public var caption1: SizeAndLeading
+    public var caption2: SizeAndLeading
+}
+
+
+extension DynamicTypeSize {
+    public var instance: DynamicTypeInstance {
+        switch self {
+        case .extraSmall:
+            .extraSmall
+        case .small:
+            .small
+        case .medium:
+            .medium
+        case .large:
+            .large
+        case .extraLarge:
+            .extraLarge
+        case .extraExtraLarge:
+            .extraExtraLarge
+        case .extraExtraExtraLarge:
+            .extraExtraExtraLarge
+        case .accessibilityMedium:
+            .accessibilityMedium
+        case .accessibilityLarge:
+            .accessibilityLarge
+        case .accessibilityExtraLarge:
+            .accessibilityExtraLarge
+        case .accessibilityExtraExtraLarge:
+            .accessibilityExtraExtraLarge
+        case .accessibilityExtraExtraExtraLarge:
+            .accessibilityExtraExtraExtraLarge
+        }
+    }
+}
---
+++ Sources/DynamicType/Values.swift
@@ -0,0 +1,169 @@
+extension DynamicTypeInstance {
+    static let extraSmall: DynamicTypeInstance = DynamicTypeInstance(
+        largeTitle: SizeAndLeading(weight: .regular, size: 31, leading: 38),
+        title1: SizeAndLeading(weight: .regular, size: 25, leading: 31),
+        title2: SizeAndLeading(weight: .regular, size: 19, leading: 24),
+        title3: SizeAndLeading(weight: .regular, size: 17, leading: 22),
+        headline: SizeAndLeading(weight: .semibold, size: 14, leading: 19),
+        body: SizeAndLeading(weight: .regular, size: 14, leading: 19),
+        callout: SizeAndLeading(weight: .regular, size: 13, leading: 18),
+        subhead: SizeAndLeading(weight: .regular, size: 12, leading: 16),
+        footnote: SizeAndLeading(weight: .regular, size: 12, leading: 16),
+        caption1: SizeAndLeading(weight: .regular, size: 11, leading: 13),
+        caption2: SizeAndLeading(weight: .regular, size: 11, leading: 13)
+    )
+
+    static let small = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 32, leading: 39),
+        title1: .init(weight: .regular, size: 26, leading: 32),
+        title2: .init(weight: .regular, size: 20, leading: 25),
+        title3: .init(weight: .regular, size: 18, leading: 23),
+        headline: .init(weight: .semibold, size: 15, leading: 20),
+        body: .init(weight: .regular, size: 15, leading: 20),
+        callout: .init(weight: .regular, size: 14, leading: 19),
+        subhead: .init(weight: .regular, size: 13, leading: 18),
+        footnote: .init(weight: .regular, size: 12, leading: 16),
+        caption1: .init(weight: .regular, size: 11, leading: 13),
+        caption2: .init(weight: .regular, size: 11, leading: 13)
+    )
+
+    static let medium: DynamicTypeInstance = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 33, leading: 40),
+        title1: .init(weight: .regular, size: 27, leading: 33),
+        title2: .init(weight: .regular, size: 21, leading: 26),
+        title3: .init(weight: .regular, size: 19, leading: 24),
+        headline: .init(weight: .semibold, size: 16, leading: 21),
+        body: .init(weight: .regular, size: 16, leading: 21),
+        callout: .init(weight: .regular, size: 15, leading: 20),
+        subhead: .init(weight: .regular, size: 14, leading: 19),
+        footnote: .init(weight: .regular, size: 12, leading: 16),
+        caption1: .init(weight: .regular, size: 11, leading: 13),
+        caption2: .init(weight: .regular, size: 11, leading: 13)
+    )
+
+    static let large = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 34, leading: 41),
+        title1: .init(weight: .regular, size: 28, leading: 34),
+        title2: .init(weight: .regular, size: 22, leading: 28),
+        title3: .init(weight: .regular, size: 20, leading: 25),
+        headline: .init(weight: .semibold, size: 17, leading: 22),
+        body: .init(weight: .regular, size: 17, leading: 22),
+        callout: .init(weight: .regular, size: 16, leading: 21),
+        subhead: .init(weight: .regular, size: 15, leading: 20),
+        footnote: .init(weight: .regular, size: 13, leading: 18),
+        caption1: .init(weight: .regular, size: 12, leading: 16),
+        caption2: .init(weight: .regular, size: 11, leading: 13)
+    )
+
+    static let extraLarge = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 36, leading: 43),
+        title1: .init(weight: .regular, size: 30, leading: 37),
+        title2: .init(weight: .regular, size: 24, leading: 30),
+        title3: .init(weight: .regular, size: 22, leading: 28),
+        headline: .init(weight: .semibold, size: 19, leading: 24),
+        body: .init(weight: .regular, size: 19, leading: 24),
+        callout: .init(weight: .regular, size: 18, leading: 23),
+        subhead: .init(weight: .regular, size: 17, leading: 22),
+        footnote: .init(weight: .regular, size: 15, leading: 20),
+        caption1: .init(weight: .regular, size: 14, leading: 19),
+        caption2: .init(weight: .regular, size: 13, leading: 18)
+    )
+
+    static let extraExtraLarge = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 38, leading: 46),
+        title1: .init(weight: .regular, size: 32, leading: 39),
+        title2: .init(weight: .regular, size: 26, leading: 32),
+        title3: .init(weight: .regular, size: 24, leading: 30),
+        headline: .init(weight: .semibold, size: 21, leading: 26),
+        body: .init(weight: .regular, size: 21, leading: 26),
+        callout: .init(weight: .regular, size: 20, leading: 25),
+        subhead: .init(weight: .regular, size: 19, leading: 24),
+        footnote: .init(weight: .regular, size: 17, leading: 22),
+        caption1: .init(weight: .regular, size: 16, leading: 21),
+        caption2: .init(weight: .regular, size: 15, leading: 20)
+    )
+
+    static let extraExtraExtraLarge = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 40, leading: 48),
+        title1: .init(weight: .regular, size: 34, leading: 41),
+        title2: .init(weight: .regular, size: 28, leading: 34),
+        title3: .init(weight: .regular, size: 26, leading: 32),
+        headline: .init(weight: .semibold, size: 23, leading: 29),
+        body: .init(weight: .regular, size: 23, leading: 29),
+        callout: .init(weight: .regular, size: 22, leading: 28),
+        subhead: .init(weight: .regular, size: 21, leading: 28),
+        footnote: .init(weight: .regular, size: 19, leading: 24),
+        caption1: .init(weight: .regular, size: 18, leading: 23),
+        caption2: .init(weight: .regular, size: 17, leading: 22)
+    )
+
+    static let accessibilityMedium = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 44, leading: 52),
+        title1: .init(weight: .regular, size: 38, leading: 46),
+        title2: .init(weight: .regular, size: 34, leading: 41),
+        title3: .init(weight: .regular, size: 31, leading: 38),
+        headline: .init(weight: .semibold, size: 28, leading: 34),
+        body: .init(weight: .regular, size: 28, leading: 34),
+        callout: .init(weight: .regular, size: 26, leading: 32),
+        subhead: .init(weight: .regular, size: 25, leading: 31),
+        footnote: .init(weight: .regular, size: 23, leading: 29),
+        caption1: .init(weight: .regular, size: 22, leading: 28),
+        caption2: .init(weight: .regular, size: 20, leading: 25)
+    )
+
+    static let accessibilityLarge = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 48, leading: 57),
+        title1: .init(weight: .regular, size: 43, leading: 51),
+        title2: .init(weight: .regular, size: 39, leading: 47),
+        title3: .init(weight: .regular, size: 37, leading: 44),
+        headline: .init(weight: .semibold, size: 33, leading: 40),
+        body: .init(weight: .regular, size: 33, leading: 40),
+        callout: .init(weight: .regular, size: 32, leading: 39),
+        subhead: .init(weight: .regular, size: 30, leading: 37),
+        footnote: .init(weight: .regular, size: 27, leading: 33),
+        caption1: .init(weight: .regular, size: 26, leading: 32),
+        caption2: .init(weight: .regular, size: 24, leading: 30)
+    )
+
+    static let accessibilityExtraLarge = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 52, leading: 61),
+        title1: .init(weight: .regular, size: 48, leading: 57),
+        title2: .init(weight: .regular, size: 44, leading: 52),
+        title3: .init(weight: .regular, size: 43, leading: 51),
+        headline: .init(weight: .semibold, size: 40, leading: 48),
+        body: .init(weight: .regular, size: 40, leading: 48),
+        callout: .init(weight: .regular, size: 38, leading: 46),
+        subhead: .init(weight: .regular, size: 36, leading: 43),
+        footnote: .init(weight: .regular, size: 33, leading: 40),
+        caption1: .init(weight: .regular, size: 32, leading: 39),
+        caption2: .init(weight: .regular, size: 29, leading: 35)
+    )
+
+    static let accessibilityExtraExtraLarge = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 56, leading: 66),
+        title1: .init(weight: .regular, size: 53, leading: 62),
+        title2: .init(weight: .regular, size: 50, leading: 59),
+        title3: .init(weight: .regular, size: 49, leading: 58),
+        headline: .init(weight: .semibold, size: 47, leading: 56),
+        body: .init(weight: .regular, size: 47, leading: 56),
+        callout: .init(weight: .regular, size: 44, leading: 52),
+        subhead: .init(weight: .regular, size: 42, leading: 50),
+        footnote: .init(weight: .regular, size: 38, leading: 46),
+        caption1: .init(weight: .regular, size: 37, leading: 44),
+        caption2: .init(weight: .regular, size: 34, leading: 41)
+    )
+
+    static let accessibilityExtraExtraExtraLarge = DynamicTypeInstance(
+        largeTitle: .init(weight: .regular, size: 60, leading: 70),
+        title1: .init(weight: .regular, size: 58, leading: 68),
+        title2: .init(weight: .regular, size: 56, leading: 66),
+        title3: .init(weight: .regular, size: 55, leading: 65),
+        headline: .init(weight: .semibold, size: 53, leading: 62),
+        body: .init(weight: .regular, size: 53, leading: 62),
+        callout: .init(weight: .regular, size: 51, leading: 60),
+        subhead: .init(weight: .regular, size: 49, leading: 58),
+        footnote: .init(weight: .regular, size: 44, leading: 52),
+        caption1: .init(weight: .regular, size: 43, leading: 51),
+        caption2: .init(weight: .regular, size: 40, leading: 48)
+    )
+}
